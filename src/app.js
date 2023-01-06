a27f765811dc0b0e2bf89d6832f89a7fa4a927ca
OniGrayFox <onigrayfox@yandex.ru>
2023-01-06 19:04:07 +0000

    Update 3 files

--- src/app.js
+++ src/app.js
@@ -3,30 +3,14 @@ import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 import Regform from '../src/layouts/Header/Header';
 import "./styles.css";
-import Regform from "RegForm"
+import Regform from "./RegForm"
 
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <input {...register("lastName")} />
-   
-      <input type="datetime" placeholder="date" {...register("date", {})} />
-     
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">chupanya-mynuanya</option>
-      </select>
-
-      <input type="submit" />
-    </form>
+    <Regform/>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Regform/>, rootElement);
